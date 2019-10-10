9f2d0eee779d4922a23049e066274fc26f789df7
Abraham Escamilla Pinelo <abrahamescamillapinelo@MacBook-Pro-de-Abraham.local>
2019-10-10 16:21:30 -0500

    Added ContactDetailViewController

---
+++ ContactsBook/ViewControllers/ContactDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactDetailViewController.swift
+//  ContactsBook
+//
+//  Created by Abraham Escamilla Pinelo on 10/10/19.
+//  Copyright © 2019 Abraham Escamilla Pinelo. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
